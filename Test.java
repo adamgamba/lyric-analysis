3ab86225c91aff27bee37b53cefd1411ccf6db06
Adam Gamba <43792793+adamgamba@users.noreply.github.com>
2021-01-13 18:33:35 -0500

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        int a = 5;
-        double b = 1.5;
-        double c = a + b;
-        System.out.println("Hello " + c);
-    }
-
-}
