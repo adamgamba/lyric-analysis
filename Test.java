7d13b417a26d4fda690a1ff5ea19cd59bda5b425
Adam Gamba <43792793+adamgamba@users.noreply.github.com>
2021-01-13 16:57:13 -0500

    Test code
    Input test code to VSCode

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 1.5;
+        double c = a + b;
+        System.out.println("Hello " + c);
+    }
+
+}
